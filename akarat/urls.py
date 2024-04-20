3d23a8ebf467afc8bceba29a46a951d0d4c46ae0
elkherchi <22025@supnum.mr>
2024-04-20 12:15:39 +0000

    push premier version

--- akarat/urls.py
+++ akarat/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import fetch_biens_immobiliers
+from .views import fetch_biens_immobiliers,sing_in,create_user,profile_list,fetch_user,biens_immobiliers_list,create_biens_immobiliers
 from django.conf.urls.static import static
 
 from django.conf import settings
 urlpatterns = [
-    # path('create_user/', create_user, name='create_user'),
-    # path('login/', sing_in, name='sing_in'),
+    path('create_user/', create_user, name='create_user'),
+    path('login/', sing_in, name='sing_in'),
 
-    # path('user_list/', profile_list, name='profile_list'),
-    # path('create_biens_immobiliers/',create_biens_immobiliers , name='create_biens_immobiliers'),
-    # path('biens_immobiliers/', biens_immobiliers_list, name='biens_immobiliers_list'),
+    path('user_list/', profile_list, name='profile_list'),
+    path('create_biens_immobiliers/',create_biens_immobiliers , name='create_biens_immobiliers'),
+    path('biens_immobiliers/', biens_immobiliers_list, name='biens_immobiliers_list'),
     path('fetch_biens_immobiliers/',fetch_biens_immobiliers, name='fetch_biens_immobiliers'),
-    # path('fetch_user/',fetch_user, name='fetch_user')
+    path('fetch_user/',fetch_user, name='fetch_user')
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
