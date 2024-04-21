365c5616d3ce6bf40db2b35a68743f5317ac389f
elkherchi <22025@supnum.mr>
2024-04-21 15:44:55 +0000

    favorie

--- akarat/urls.py
+++ akarat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import fetch_biens_immobiliers,sing_in,create_user,profile_list,fetch_user,biens_immobiliers_list,create_biens_immobiliers
+from .views import fetch_biens_immobiliers,favorie_biens,sing_in,create_user,profile_list,fetch_user,biens_immobiliers_list,create_biens_immobiliers
 from django.conf.urls.static import static
 
 from django.conf import settings
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', sing_in, name='sing_in'),
 
     path('user_list/', profile_list, name='profile_list'),
+    path('favorie/', favorie_biens, name='favorie_biens'),
     path('create_biens_immobiliers/',create_biens_immobiliers , name='create_biens_immobiliers'),
     path('biens_immobiliers/', biens_immobiliers_list, name='biens_immobiliers_list'),
     path('fetch_biens_immobiliers/',fetch_biens_immobiliers, name='fetch_biens_immobiliers'),
